6ee06af39fd79884e12e69534414a40b18a20a4a
darellanodev <darellano.aliaga@gmail.com>
2026-06-10 23:07:50 +0200

    fix(Door): ensure all child layers are disabled upon deactivation

--- src/Door.ts
+++ src/Door.ts
@@ -17,5 +17,8 @@ export class Door {
   deactivate() {
     this.active = false
     this.doorObject.visible = false
+    this.doorObject.traverse((child) => {
+      child.layers.disableAll()
+    })
   }
 }
